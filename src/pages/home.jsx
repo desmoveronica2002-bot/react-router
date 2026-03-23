cbe99487765ff857c7c05157d148c7344588b662
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-03-23 20:11:03 +0100

    chi siamo

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -26,11 +26,6 @@ export default function Home(){
             <img src="https://nuvoluzione.com/wp-content/uploads/2016/03/vendita-promozionale.jpg" alt="img promozionale" />
 
         </main>
-
-        <footer>
-            <h2>fine</h2>
-        </footer>
-          
           </>
     )
 }
--- src/pages/presentazione.jsx
+++ src/pages/presentazione.jsx
@@ -1,8 +1,34 @@
 export default function Presentazione(){
     return(
         <>
-          <h1>BENVENUTO!</h1>
-          <img src="https://nuvoluzione.com/wp-content/uploads/2016/03/vendita-promozionale.jpg" alt="img promozionale" />
-        </>
+        <header> 
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+               <div class="container-fluid">
+                  <div class="collapse navbar-collapse" id="navbarNav">
+                   <ul class="navbar-nav">
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="/presentazione">chi siamo</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="products">prodotti</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+            </nav>
+        </header>
+
+        <main>
+            <h1>CHI SIAMO!</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero sapiente 
+            dolores ipsum ipsa? Minima assumenda dolores quod minus aspernatur ducimus?
+            Doloremque similique eum voluptates velit soluta ipsum, officiis sed porro.</p>
+
+        </main>
+          
+          </>
     )
 }
