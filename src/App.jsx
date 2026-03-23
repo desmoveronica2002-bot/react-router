25cf95676cbc42874b45766ef539a97f9b98359c
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-03-23 17:34:41 +0100

    create element

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import {BrowsetRouter, Routes} from "react-router-dom"
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Home from "./pages/home";
+import Presentazione from "./pages/presentazione";
+import Products from "./pages/products";
 
 function App() {
 
   return (
     <>
-    <BrowsetRouter>
+    <BrowserRouter>
     <Routes> 
-      <Routes path="/" element={<h1>Homepage</h1>}/>
-      <Routes path="/presentazione" element={<h1>chi siamo</h1>}/>
-      <Routes path="/products" element={<h1>prodotti</h1>}/>
-
+      <Route path="/" element={<h1>Homepage</h1>}/>
+      <Route path="/presentazione" element={<h1>chi siamo</h1>}/>
+      <Route path="/products" element={<h1>prodotti</h1>}/>
     </Routes>
-    </BrowsetRouter>
+    </BrowserRouter>
 
     </>
   )
